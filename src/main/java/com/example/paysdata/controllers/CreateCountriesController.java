9d6ec28165d1d82d0f6e3e0fae50765bd8a119d4
Sofiane <sofiane.ali.nice2@gmail.com>
2019-06-05 16:09:37 +0200

    Creation du CreateCountriesController
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/example/paysdata/controllers/CreateCountriesController.java
@@ -0,0 +1,50 @@
+package com.example.paysdata.controllers;
+
+import com.example.paysdata.entity.Pays;
+import com.example.paysdata.service.PaysServiceMysql;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/createCountries")
+public class CreateCountriesController {
+    @Autowired
+    private PaysServiceMysql paysServiceMusql;
+
+    @GetMapping(value="" , produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String initService(Model model) {
+    ArrayList<String> countryCodes = new ArrayList<>();
+    countryCodes.add("FRA");
+    String json = getJsonOfCountry(countryCodes.get(0));
+    System.out.println(json);
+    return json;
+
+    }
+
+    private String getJsonOfCountry(String countryCode) {
+        String str ="";
+        String jsonResult = "";
+        try {
+            URL url = new URL("https://restcountries.eu/rest/v2/alpha/" + countryCode);
+            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+            while (null != (str = br.readLine())) {
+                jsonResult += str;
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return jsonResult;
+    }
+
+}
