788dcb1bdd4151958a59ee8e8e6d3568d8bebe17
Jemli Mohamed Elarbi <youremail@example.com>
2019-06-05 14:41:57 +0200

    add fonction
    # Conflicts:
    #	src/main/resources/static/index.html

---
+++ src/main/java/com/example/paysdata/dao/PaysDaoCrudRepo.java
@@ -0,0 +1,8 @@
+package com.example.paysdata.dao;
+
+import com.example.paysdata.entity.Pays;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PaysDaoCrudRepo extends CrudRepository<Pays, Integer> {
+
+}
