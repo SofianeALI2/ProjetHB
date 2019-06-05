9c2bc1970f0e43c9706a2f487e1dee3de2ff7472
Jemli Mohamed Elarbi <youremail@example.com>
2019-06-05 10:20:14 +0200

    initialisation du projet

---
+++ src/main/java/com/example/paysdata/PaysdataApplication.java
@@ -0,0 +1,13 @@
+package com.example.paysdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaysdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaysdataApplication.class, args);
+    }
+
+}
