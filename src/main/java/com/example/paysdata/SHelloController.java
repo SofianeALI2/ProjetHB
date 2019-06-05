4e7ffddcf778ef351846fe5dd2d97ad17d685881
Sofiane <sofiane.ali.nice2@gmail.com>
2019-06-05 12:21:19 +0200

    Ajout des fichier index et pays.html avec les dossier css,img et js dans static
    # Conflicts:
    #	.gitignore

--- src/main/java/com/example/paysdata/SHelloController.java
+++ src/main/java/com/example/paysdata/SHelloController.java
@@ -1,14 +1,16 @@
 package com.example.paysdata;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
+@RequestMapping("/")
 public class SHelloController {
 
-    @RequestMapping("/")
+    @GetMapping("")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "index";
     }
 
 }
